22e7b800c346ca5f6d35b9a1853d80630a68e0b0
Santhosh Ramanathan <santhoshramanathan25@gmail.com>
2021-05-11 14:16:42 +0530

    added code to support mashup response

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var password="change-password";
 var url = "https://150.238.99.51:9443/fhir-server/api/v4/";
 var auth = "Basic "+new Buffer(username + ":" + password).toString("base64");
 
+
 const headers = {
 	"Authorization" : auth, rejectUnauthorized: false, requestCert: true, agent: false
 }
@@ -18,9 +19,20 @@ app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
 
+app.get('/queryparams', (req, res) => {
+	if(req.query.patientid){
+	res.send('query params values: '+ req.query.patientid);
+	}
+	else{
+	res.send('no query params');
+	}
+});
+
 app.get('/:operation', (req, res) => {
-  Request.get({
-	  url : url+req.params.operation,
+  
+  if(req.query.patient) {
+	 Request.get({
+	  url : url+req.params.operation+'?patient='+req.query.patient,
 	  headers : { "Authorization" : auth},
 	  rejectUnauthorized: false,
 	  requestCert: true,
@@ -30,9 +42,74 @@ app.get('/:operation', (req, res) => {
 		res.send(error);
         return console.dir(error);
     }
-    
 	res.send(JSON.parse(body));
-});
+  });
+  }
+  else if(req.query.encounter)
+  {
+	 Request.get({
+	  url : url+req.params.operation+'?encounter='+req.query.encounter,
+	  headers : { "Authorization" : auth},
+	  rejectUnauthorized: false,
+	  requestCert: true,
+	  agent: false
+}, (error, response, body) => {
+    if(error) {
+		res.send(error);
+        return console.dir(error);
+    }
+	res.send(JSON.parse(body));
+  });
+  }
+  else if(req.query.practitioner)
+  {
+	 Request.get({
+	  url : url+req.params.operation+'?practitioner='+req.query.practitioner,
+	  headers : { "Authorization" : auth},
+	  rejectUnauthorized: false,
+	  requestCert: true,
+	  agent: false
+}, (error, response, body) => {
+    if(error) {
+		res.send(error);
+        return console.dir(error);
+    }
+	res.send(JSON.parse(body));
+  });
+  }
+  else if(req.query.recorder)
+  {
+	 Request.get({
+	  url : url+req.params.operation+'?recorder='+req.query.recorder,
+	  headers : { "Authorization" : auth},
+	  rejectUnauthorized: false,
+	  requestCert: true,
+	  agent: false
+}, (error, response, body) => {
+    if(error) {
+		res.send(error);
+        return console.dir(error);
+    }
+	res.send(JSON.parse(body));
+  });
+  }
+  else 
+  {
+	  Request.get({
+		  url : url+req.params.operation,
+		  headers : { "Authorization" : auth},
+		  rejectUnauthorized: false,
+		  requestCert: true,
+		  agent: false
+	}, (error, response, body) => {
+		if(error) {
+			res.send(error);
+			return console.dir(error);
+		}
+		
+		res.send(JSON.parse(body));
+	});
+  }
 });
 
 app.get('/:operation/:id', (req, res) => {
